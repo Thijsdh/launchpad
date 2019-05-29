7c6a2c0e067641bbe21edfca5e3a4d8f5c686ccc
Thijs den Hartog <Thijs@thijsdh.nl>
2019-05-29 22:17:06 +0200

    Update theme color

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const manifest = require('./public/manifest.json');
+
+pwaArgs = {
+	msTileColor: manifest.background_color,
+	name: manifest.short_name,
+	themeColor: manifest.theme_color
+};
+
 module.exports = {
-	publicPath: '/launchpad'
+	publicPath: '/launchpad',
+	chainWebpack: config => {
+		config.plugin('pwa').tap(() => {
+			return [pwaArgs];
+		});
+	}
 };
